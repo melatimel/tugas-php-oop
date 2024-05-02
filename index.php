de7546abcdb4a1698fe68b14bed49b84ebebc954
destibarrotun07 <destibarotun@gmail.com>
2024-05-02 09:07:23 +0700

    melati

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "frog.php";
+    require_once "ape.php";
+
+
+    $sheep = new animal("Shaun");
+    echo "Name : " .$sheep-> get_name(); //"shaun"
+    echo "<br/> Legs : " .$sheep-> get_legs(); //4
+    echo "<br/> Cold Blooded : " .$sheep-> get_cold_blooded(); //"no"
+
+    echo "<br/>";
+    echo "<br/>";
+
+    $kodok = new Frog("Buduk");
+    $kodok->getFrogName();
+    $kodok->getFrogLegs();
+    $kodok->getFrogBlood();
+    $kodok->getFrogJump();
+
+    echo "<br/>";
+    echo "<br/>";
+    
+    $ape = new Ape("Kera Sakti");
+    $ape->getApeName();
+    $ape->getApeLegs();
+    $ape->getApeBlood();
+    $ape->getApeYell();
+?>
